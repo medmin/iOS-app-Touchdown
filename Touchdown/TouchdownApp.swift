99fcfdf8ba4556f85cb83ad61eba63c852fcbb81
Yumin Gui <yumindev@gmail.com>
2021-07-31 09:36:16 -0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Yumin Gui on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
